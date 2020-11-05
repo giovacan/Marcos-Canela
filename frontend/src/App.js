9d30154034c760d71f7147e6a5f9c7601a78027c
Eli <cyronix787@gmail.com>
2020-11-05 21:57:44 +0000

    comment apollo code out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
-import { ApolloProvider } from 'react-apollo'
-import ApolloClient from 'apollo-boost'
+// import { ApolloProvider } from 'react-apollo'
+// import ApolloClient from 'apollo-client'
 
 //icons
 
@@ -13,24 +13,26 @@ import Pye from './components/Pye/promosandevents'
 import Lph from './components/Lastproductshome/Lph'
 import Foot from './components/Foot/foot'
 
-const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql',
-	onerror: ({ networkError, graphQLErrors }) => {
-		console.log('graphQLErrors', graphQLErrors)
-		console.log('networkError', networkError)
-	}
-})
+// const client = new ApolloClient({
+// 	uri: 'http://localhost:4000/graphql',
+// 	onerror: ({ networkError, graphQLErrors }) => {
+// 		console.log('graphQLErrors', graphQLErrors)
+// 		console.log('networkError', networkError)
+// 	}
+// })
 
 function App() {
 	return (
-		<ApolloProvider client={client}>
+		// <ApolloProvider client={client}>
+		<>
 			<Navbar />
 			<Showcase />
 			<Newscards />
 			<Pye />
 			<Lph carouselId='carousel' />
 			<Foot />
-		</ApolloProvider>
+		</>
+		// </ApolloProvider>
 	)
 }
 
