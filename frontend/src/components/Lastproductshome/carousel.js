7faff7f5f19c6656882d6328b794b1a300516b4c
Eli <cyronix787@gmail.com>
2020-11-05 22:22:31 +0000

    add comments

--- frontend/src/components/Lastproductshome/carousel.js
+++ frontend/src/components/Lastproductshome/carousel.js
@@ -7,26 +7,32 @@ import { useState, useEffect } from 'react'
  * @param {Number} numberOfSlides Number of slides to show
  */
 function useCarousel(id, numberOfSlides) {
+	// Elements state for storing values od elements
 	const [elements, setElements] = useState({
 		carousel: null
 	})
 
+	// Values state for translation effect and changing slides
 	const [values, setValues] = useState({
 		offset: 0
 	})
 
+	// An effect that sets the value of the carousel element into state
 	useEffect(() => {
 		const carousel = document.querySelector(`#${id}`)
 		setElements({ carousel })
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
+	// Handles translation/change of slides
 	const handleSlideChange = () => {
 		elements.carousel.style.transform = `translateX(${values.offset}%)`
 	}
 
+	// An effect that triggers when values state changes
 	useEffect(handleSlideChange, [elements.carousel, values])
 
+	// Handles the click function of the left controller
 	const handleLeftControlClick = () => {
 		const { offset } = values
 		setValues({
@@ -34,6 +40,7 @@ function useCarousel(id, numberOfSlides) {
 		})
 	}
 
+	// Handles the click function of the right controller
 	const handleRightControlClick = () => {
 		const { offset } = values
 		setValues({
