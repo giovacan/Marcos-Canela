9949ab13a810b1d96e1db9da1e8b7764eb60ec3e
Eli <cyronix787@gmail.com>
2020-11-06 07:06:40 +0000

    use useCarousel hook

--- frontend/src/components/Lastproductshome/Slider.js
+++ frontend/src/components/Lastproductshome/Slider.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import useCarousel from './carousel'
 
-function Slider() {}
+function Slider({ id, children }) {
+	const numberOfSlides = children ? 1 : children.length || null
+	const carousel = useCarousel(id, numberOfSlides)
+	return (
+		<div className='slider'>
+			<div className='slider-box'>
+				<div className='slide'></div>
+			</div>
+		</div>
+	)
+}
 
 export default Slider
