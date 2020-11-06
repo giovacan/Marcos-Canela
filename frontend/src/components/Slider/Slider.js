ff1face89b9143ef911a0a537904f3f106646dd2
Eli <cyronix787@gmail.com>
2020-11-06 07:55:27 +0000

    add transition delay

--- frontend/src/components/Slider/Slider.js
+++ frontend/src/components/Slider/Slider.js
@@ -17,7 +17,7 @@ function Slider({ id, children }) {
 						<Leftcontroller id='previous' />
 					</button>
 				</div>
-				<div id={id} className='slides'>
+				<div id={id} className='slides' style={{ transitionDelay: '0.3s' }}>
 					{children}
 				</div>
 				<div className='control-box next'>
