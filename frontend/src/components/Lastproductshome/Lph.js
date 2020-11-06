9347a7d6a596a039b392171e7cbf85eec9209974
Eli <cyronix787@gmail.com>
2020-11-06 07:16:40 +0000

    use slides

--- frontend/src/components/Lastproductshome/Lph.js
+++ frontend/src/components/Lastproductshome/Lph.js
@@ -9,6 +9,7 @@ import Img1 from './img/fimgps.jpg'
 
 // Import slider component
 import Slider from './slider/Slider'
+import Slide from './slider/Slide'
 
 // carousel funtion
 
@@ -19,73 +20,73 @@ function Lph({ carouselId }) {
 			<div class='titlelph'>
 				<h3>Title</h3>
 			</div>
-			<Slider id={carouselId}></Slider>
-			<div class='featured-products'>
-				<div className='slide-container'>
-					<div className='carousel'>
-						<div className='product 1'>
-							<img src={Img1} className='img1' alt='' />
-							<h2 className='titleproduct'>Hola mundo</h2>
-							<p className='description-product'>Precio: $100.00 MXN</p>
-							<div className='btns-product'>
-								<div className='details'>
-									<div className='btn-detail'>Detalles</div>
-								</div>
-								<div className='add-to-cart'>
-									<div className='btn-add-cart'>
-										<i class='fas fa-shopping-cart' aria-hidden='true' size='x2'></i>
-									</div>
-								</div>
+			<Slider id={carouselId}>
+				<Slide>
+					<div className='product 1'>
+						<img src={Img1} className='img1' alt='' />
+						<h2 className='titleproduct'>Hola mundo</h2>
+						<p className='description-product'>Precio: $100.00 MXN</p>
+						<div className='btns-product'>
+							<div className='details'>
+								<div className='btn-detail'>Detalles</div>
 							</div>
-						</div>
-						<div className='product 2'>
-							<img src={Img1} className='img1' alt='' />
-							<h2 className='titleproduct'>Hola mundo</h2>
-							<p className='description-product'>Precio: $100.00 MXN</p>
-							<div className='btns-product'>
-								<div className='details'>
-									<div className='btn-detail'>Detalles</div>
-								</div>
-								<div className='add-to-cart'>
-									<div className='btn-add-cart'>
-										<i class='fas fa-shopping-cart' aria-hidden='true' size='x2'></i>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className='product 3'>
-							<img src={Img1} className='img1' alt='' />
-							<h2 className='titleproduct'>Hola mundo</h2>
-							<p className='description-product'>Precio: $100.00 MXN</p>
-							<div className='btns-product'>
-								<div className='details'>
-									<div className='btn-detail'>Detalles</div>
-								</div>
-								<div className='add-to-cart'>
-									<div className='btn-add-cart'>
-										<i class='fas fa-shopping-cart' aria-hidden='true' size='x2'></i>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div className='product 4'>
-							<img src={Img1} className='img1' alt='' />
-							<h2 className='titleproduct'>Hola mundo</h2>
-							<p className='description-product'>Precio: $100.00 MXN</p>
-							<div className='btns-product'>
-								<div className='details'>
-									<div className='btn-detail'>Detalles</div>
-								</div>
-								<div className='add-to-cart'>
-									<div className='btn-add-cart'>
-										<i class='fas fa-shopping-cart' aria-hidden='true' size='x2'></i>
-									</div>
+							<div className='add-to-cart'>
+								<div className='btn-add-cart'>
+									<i class='fas fa-shopping-cart' aria-hidden='true' size='x2'></i>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div>
+					<div className='product 2'>
+						<img src={Img1} className='img1' alt='' />
+						<h2 className='titleproduct'>Hola mundo</h2>
+						<p className='description-product'>Precio: $100.00 MXN</p>
+						<div className='btns-product'>
+							<div className='details'>
+								<div className='btn-detail'>Detalles</div>
+							</div>
+							<div className='add-to-cart'>
+								<div className='btn-add-cart'>
+									<i class='fas fa-shopping-cart' aria-hidden='true' size='x2'></i>
+								</div>
+							</div>
+						</div>
+					</div>
+				</Slide>
+				<Slide>
+					<div className='product 3'>
+						<img src={Img1} className='img1' alt='' />
+						<h2 className='titleproduct'>Hola mundo</h2>
+						<p className='description-product'>Precio: $100.00 MXN</p>
+						<div className='btns-product'>
+							<div className='details'>
+								<div className='btn-detail'>Detalles</div>
+							</div>
+							<div className='add-to-cart'>
+								<div className='btn-add-cart'>
+									<i class='fas fa-shopping-cart' aria-hidden='true' size='x2'></i>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='product 4'>
+						<img src={Img1} className='img1' alt='' />
+						<h2 className='titleproduct'>Hola mundo</h2>
+						<p className='description-product'>Precio: $100.00 MXN</p>
+						<div className='btns-product'>
+							<div className='details'>
+								<div className='btn-detail'>Detalles</div>
+							</div>
+							<div className='add-to-cart'>
+								<div className='btn-add-cart'>
+									<i class='fas fa-shopping-cart' aria-hidden='true' size='x2'></i>
+								</div>
+							</div>
+						</div>
+					</div>
+				</Slide>
+			</Slider>
+			<div class='featured-products'></div>
 		</div>
 	)
 }
