bccf993ce322067c090ef767742c688712f65035
Eli <cyronix787@gmail.com>
2020-11-07 08:15:13 +0000

    add media query function

--- frontend/src/components/Slider/useResponsiveSlides.js
+++ frontend/src/components/Slider/useResponsiveSlides.js
@@ -1,3 +1,31 @@
-function useResponsiveSlides() {}
+import { useState } from 'react'
+import Slide from './Slide'
+
+export const useMatches = query => {
+	const [isMatch, setIsMatch] = useState(matchMedia(query).matches)
+	matchMedia(query).onchange = () => {
+		setIsMatch(matchMedia(query).matches)
+	}
+	return isMatch
+}
+
+const breakpoints = {
+	xs: '(min-width: 0px) and (max-width: 600px)',
+	sm: '(min-width: 601px) and (max-width: 900px)',
+	md: '(min-width: 901px) and (max-width: 800px)',
+	lg: '(min-width: 801px)'
+}
+
+function useResponsiveSlides(children) {
+	let totalChildren = []
+	if (Array.isArray(children)) {
+		children.forEach(({ props }) => {
+			totalChildren = totalChildren.concat(props.children)
+		})
+	} else {
+		totalChildren = totalChildren.concat(children.props.children)
+	}
+	return totalChildren.map((children, index) => <Slide key={index}>{children}</Slide>)
+}
 
 export default useResponsiveSlides
